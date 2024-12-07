e35ae137a9e208726b747cfcbb8d0e9ead3d0e16
DNahar74 <nahardarsh55@gmail.com>
2024-12-07 13:08:21 +0530

    initialization of server

---
+++ server/types.ts
@@ -0,0 +1,21 @@
+//todo: (1) Understand each of these socket.io types
+//todo: (2) Divide the types into different files
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
