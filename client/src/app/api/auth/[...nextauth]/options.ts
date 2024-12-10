2118d4d996742099d21e5159048ab99c2ba617c5
DNahar74 <nahardarsh55@gmail.com>
2024-12-10 23:23:10 +0530

    added basic auth-options, configured with google console

--- client/src/app/api/auth/[...nextauth]/options.ts
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,5 @@
+//todo: (1) Create callbacks for signin, signout and signup methods
+
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -8,5 +10,32 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      //? This assigns all the properties of user to token
+      if (user) {
+        // token.email = user.email;
+      }
+
+      return token
+    },
+    async session({ session, token }) {
+      //? This assigns all the properties of token to a user object in session
+      if (token) {
+        // session.user.email = token.email;
+      }
+
+      return session
+    },
+  },
+  pages: {
+    newUser: '/signup',
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error'
+  },
+  session: {
+    strategy: 'jwt'
+  },
   secret: process.env.NEXTAUTH_SECRET
 }
