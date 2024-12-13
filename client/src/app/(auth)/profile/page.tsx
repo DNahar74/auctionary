5ca538bbc9d7baaf2dcf47f36fa0faaaa7637a6b
DNahar74 <nahardarsh55@gmail.com>
2024-12-13 22:56:07 +0530

    added profile page

---
+++ client/src/app/(auth)/profile/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <>
+    <Navbar />
+    <div>Profile</div>
+    </>
+  )
+}
+
+export default Profile
